22b9361c7464c315de3b8173d27150411eceb3d4
Dmitriy Gallyamov <dmitrijs.gallamovs@gmail.com>
2019-03-10 19:43:47 +0200

    SUPPORT-127 Add notice for PayPal Plus invoice

---
+++ app/code/local/DevAll/AuItPdf/Model/Offer.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Class DevAll_AuItPdf_Model_Offer
+ */
+class DevAll_AuItPdf_Model_Offer extends AuIt_Pdf_Model_Offer
+{
+    /**
+     * @return mixed
+     */
+    protected function getBuildAfterTableText()
+    {
+        $variables = $this->_processor->getVariables();
+        $invoice = isset($variables['invoice']) ? $variables['invoice'] : false;
+        $payment = $invoice ? $invoice->getOrder()->getPayment() : false;
+
+        $this->_processor->setVariables([
+            'ppp_pui_instruction_type' => $payment->getData('ppp_pui_instruction_type'),
+            'ppp_pui_account_holder_name' => $payment->getData('ppp_pui_account_holder_name'),
+            'ppp_pui_international_bank_account_number' => $payment->getData('ppp_pui_international_bank_account_number'),
+            'ppp_pui_bank_identifier_code' => $payment->getData('ppp_pui_bank_identifier_code')
+        ]);
+
+        return parent::getBuildAfterTableText();
+    }
+}
