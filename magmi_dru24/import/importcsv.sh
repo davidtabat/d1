24afea61f7f0615e6de1ae62cb2094c6aed6313c
sandro768 <sandromirijanashvili@gmail.com>
2019-05-30 12:31:13 +0400

    Reindexing after products disable

--- magmi_dru24/import/importcsv.sh
+++ magmi_dru24/import/importcsv.sh
@@ -5,5 +5,5 @@ PROJECT_ROOT=$2
 IMPORT_START=$(date +%s)
 
 ${PHP_PATH} ${PROJECT_ROOT}/magmi_dru24/cli/magmi.cli.php -chain=Samsung:create,Kyocera:create,Epson:create,Lg:create
-${PHP_PATH} ${PROJECT_ROOT}/shell/indexer.php -- reindexall
 ${PHP_PATH} ${PROJECT_ROOT}/shell/devAll_disableProducts.php --import_start ${IMPORT_START}
+${PHP_PATH} ${PROJECT_ROOT}/shell/indexer.php -- reindexall
